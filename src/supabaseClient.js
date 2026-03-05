b7b181771777b02efc0e4f0651599b10fcd16843
carolprillo1995-eng <carolprillo1995@gmail.com>
2026-03-05 19:28:58 -0300

    Força src correto

---
+++ src/supabaseClient.js
@@ -0,0 +1,6 @@
+import { createClient } from "@supabase/supabase-js";
+
+const SUPABASE_URL = "https://qqgoojzlhczfexqlgvpe.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFxZ29vanpsaGN6ZmV4cWxndnBlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI2ODM0ODQsImV4cCI6MjA4ODI1OTQ4NH0.C_rElTl676HaMHzkrJMPAkcm58edODGSJzvpu4xaDa0";
+
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
